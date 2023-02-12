1569b73911957c072fe3b11a6ed676fd6a2d913d
Asutosh Pradhan <75766416+Maagztech@users.noreply.github.com>
2023-02-12 21:25:13 +0530

    final commit just minor changes left

--- web/Backend/server/controllers/authCtrl.ts
+++ web/Backend/server/controllers/authCtrl.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+const request = require("request");
 import Users from "../models/userModel";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
@@ -110,14 +111,14 @@ const authCtrl = {
   },
   login: async (req: Request, res: Response) => {
     try {
-      const { email, buffer } = req.body;
+      const { email, password } = req.body;
 
       const user = await Users.findOne({ email });
       if (!user)
         return res.status(400).json({ msg: "This account does not exits." });
 
       // if user exists
-      loginUser(user, buffer, res);
+      loginUser(user, password, res);
     } catch (err: any) {
       return res.status(500).json({ msg: err.message });
     }
@@ -181,51 +182,9 @@ const loginUser = async (user: IUser, password: string, res: Response) => {
   }
 
   const access_token = generateAccessToken({ id: user._id });
-  const refresh_token = generateRefreshToken({ id: user._id }, res);
-
-  await Users.findOneAndUpdate(
-    { _id: user._id },
-    {
-      rf_token: refresh_token,
-    }
-  );
-  const client = await Users.aggregate([
-    {
-      $match: {
-        _id: user._id,
-      },
-    },
-    {
-      $project: {
-        avatar: 1,
-        role: 1,
-        type: 1,
-        name: 1,
-        account: 1,
-        password: "",
-        createdAt: 1,
-        updatedAt: 1,
-        rf_token: 1,
-        about: 1,
-        follower: {
-          $size: "$follower",
-        },
-        following: {
-          $size: "$following",
-        },
-        blogcount: 1,
-        referer: 1,
-        notice: 1,
-        preferance: 1,
-        followinglist: "$following",
-      },
-    },
-  ]);
 
   return res.json({
     msg: "Login Success!",
-    access_token,
-    user: client[0],
   });
 };
 
@@ -236,14 +195,6 @@ const registerUser = async (user: IUserParams, res: Response) => {
   const refresh_token = generateRefreshToken({ id: newUser._id }, res);
 
   const regUser = await newUser.save();
-  notificationCtrl.addNotification(
-    regUser._id,
-    "Welcome! to Pediageeks world.",
-    "Hii! " +
-      " " +
-      regUser.name +
-      " on behalf of whole pediageek team we welcome you to the platform. Create your First blog and earn 200-250 Rs. on evry 1000 Views on your blog."
-  );
   res.json({
     msg: "Registration success!",
     access_token,
